d0171fe793212305600a3fcb299e2f550c73aae3
tkdgns0630 <tkdgns0630@gmail.com>
2023-10-06 17:20:16 +1100

    finished install click handler and webpack config

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,31 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {    
+     // save the prompt event for later
+     window.deferredPrompt = event;
+     // if button is clicked the hidden class will be removed and toggle stops.
+     butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+     // save deferredprompt event to a variable
+     const promptEvent = window.deferredPrompt;
+     // if it there is no saved event return
+     if (!promptEvent) {
+      return;
+     }
+     // runs the prompt event
+     promptEvent.prompt();
+     // delete the deferred event from window
+     window.deferredPrompt = null;
+     // remove the button by giving it hidden.
+     butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// deferred event deleted when installed.
+window.addEventListener('appinstalled', (event) => {
+     window.deferredPrompt = null;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+// Service worker Plugin
+const { GenerateSW } = require("workbox-webpack-plugin");
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,12 +21,52 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor',
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }), 
+      new WebpackPwaManifest({
+        name: 'Text editor',
+        short_name: 'TE',
+        description: 'My awesome Progressive Web Text editor!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          },
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          // css loader
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          // barbell loader
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
